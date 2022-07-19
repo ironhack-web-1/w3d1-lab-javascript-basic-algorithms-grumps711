d1cafc6ff3e1b9c4815b1a6d7fbb940878edeee3
grumpycat711 <eugeni.puigdomenech@gmail.com>
2022-07-19 20:30:56 +0200

    Iteration 1: Names and Input

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // Iteration 1: Names and Input
 //
+console.log("I'm ready!");
+
+let hacker1="Pepito";
+console.log("The driver's name is "+hacker1);
+let hacker2="Grillo";
+console.log("The navigator's name is "+hacker2);
+
 // Iteration 2: Conditionals
+
+
 
 
 // Iteration 3: Loops
